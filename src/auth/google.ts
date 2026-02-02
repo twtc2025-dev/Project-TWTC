9ea31abc3a5819e6fb2cd048c3a672cf3829de30
twtc2025-dev <twtc.2025@gmail.com>
2026-02-02 21:33:53 +0100

    Create google.ts

---
+++ src/auth/google.ts
@@ -0,0 +1,19 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL!,
+    },
+    async (_accessToken, _refreshToken, profile, done) => {
+      // هنا تخزن المستخدم في DB أو ترجع profile
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
